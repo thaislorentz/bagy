c64e9114397f3796861a39fe62f743244aeed0e2
thaislorentzz <thais@hivesys.com.br>
2021-11-28 11:41:30 -0300

    Visão Geral page

---
+++ src/components/Chart/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactChart from "react-apexcharts";
+
+import './style.scss';
+
+const Chart = () => {
+
+  const horasExtras = [{
+    name: 'TEAM A',
+    type: 'area',
+    data: [44, 55, 31, 47, 31, 43, 26, 41, 31, 47, 33]
+  }, {
+    name: 'TEAM B',
+    type: 'line',
+    data: [55, 69, 45, 61, 43, 54, 37, 52, 44, 61, 43]
+  }]
+  const optionsHorasTrabalhadas = {
+    chart: {
+      height: 350,
+      type: 'line',
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    fill: {
+      type: 'solid',
+      opacity: [0.35, 1],
+    },
+    labels: ['Dec 01', 'Dec 02', 'Dec 03', 'Dec 04', 'Dec 05', 'Dec 06', 'Dec 07', 'Dec 08', 'Dec 09 ', 'Dec 10', 'Dec 11'],
+    markers: {
+      size: 0
+    },
+    yaxis: [
+      {
+        title: {
+          text: 'Series A',
+        },
+      },
+      {
+        opposite: true,
+        title: {
+          text: 'Series B',
+        },
+      },
+    ],
+    tooltip: {
+      shared: true,
+      intersect: false,
+      y: {
+        formatter: function (y) {
+          if (typeof y !== "undefined") {
+            return y.toFixed(0) + " points";
+          }
+          return y;
+        }
+      }
+    }
+  }
+
+  return (
+    <div className="chart">
+      <ReactChart options={optionsHorasTrabalhadas} series={horasExtras} type="line" height={350} />
+
+    </div>
+  );
+}
+
+export default Chart;
--- src/pages/Geral/index.js
+++ src/pages/Geral/index.js
@@ -1,13 +1,62 @@
 import React from "react"
 import Header from '../../components/Header'
+import Card from '../../components/Card'
+import Chart from '../../components/Chart'
 import "./style.scss";
 
 const Geral = () => {
 
   return (
-    <>
-      <Header page="Visão Geral"/>
-    </>
+    <div className="geral">
+      <Header page="Visão Geral" />
+      <div className="geral-container">
+        <div className="geral-cards">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className="geral-content">
+          <div className="geral-chart">
+            <h3 className="geral-chart-title">Total de faturamento mensal</h3>
+            <span className="geral-chart-mounth">JULHO 2020</span>
+            <div>
+              <Chart />
+            </div>
+          </div>
+          <div className="geral-details">
+            <div className="geral-details-data">
+              <span className="geral-details-label">Loja</span>
+              <h3 className="geral-details-title">Estilo Pri</h3>
+            </div>
+            <div className="geral-details-data">
+              <span className="geral-details-label">Loja</span>
+              <h3 className="geral-details-title">Estilo Pri</h3>
+            </div>
+            <div className="geral-details-data">
+              <span className="geral-details-label">Loja</span>
+              <h3 className="geral-details-title">Estilo Pri</h3>
+            </div>
+            <div className="geral-details-data">
+              <span className="geral-details-label">Loja</span>
+              <h3 className="geral-details-title">Estilo Pri</h3>
+            </div>
+            <div className="geral-details-data">
+              <span className="geral-details-label">Loja</span>
+              <h3 className="geral-details-title">Estilo Pri</h3>
+            </div>
+          </div>
+        </div>
+        <div>
+        <div className="geral-content">
+          oi
+        </div>
+        <div className="geral-content">
+          oi
+        </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
