fe79136c8eb57c60280290e96436941a07f2ba20
thaislorentzz <thais@hivesys.com.br>
2021-11-28 11:41:12 -0300

    Card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.scss';
+
+const Card = () => {
+  return (
+    <div className="card">
+      <span className="card-title">Faturamento total</span>
+      <span className="card-data">100.000,00</span>
+    </div>
+  );
+}
+
+export default Card;
